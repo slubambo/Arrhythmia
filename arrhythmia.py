75e62f256745e417ba4e955a3d5b239db2807840
slubambo <slubambo57@gmail.com>
2023-03-03 11:34:15 +0300

    :construction: Exploratory Analysis

--- arrhythmia.py
+++ arrhythmia.py
@@ -6,4 +6,15 @@ import seaborn as sb
 #Load Dataset 
 df = pd.read_csv(r'arrhythmia.csv')
 
-print(df)
+#Exploratory Analysis
+#print(df.shape)
+
+#print(df.columns)
+
+#print(df.head(10)) #Printing first 10 entries of the dataset
+
+#print(df.info()) # Understanding the different datasets
+
+#print(df.isna().sum()) # Getting null values
+
+print(df.describe()) #show the summary statistics of the numeric variables.
